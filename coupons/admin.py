0dfa474a45c8db4bd1dfbb5c4c7477e3df264dd7
Maaz Abdul Basith <maazabdulbasith@gmail.com>
2025-09-29 17:57:43 +0530

    chore(admin): hide builtin auth Group from admin sidebar

--- coupons/admin.py
+++ coupons/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import Coupon
+from django.contrib.auth.models import Group
+from django.contrib.admin.sites import NotRegistered
 
 @admin.register(Coupon)
 class CouponAdmin(admin.ModelAdmin):
     list_display = ('code','discount_type','discount_value','active','expiry_date','usage_limit','used_count')
     search_fields = ('code',)
     list_filter = ('active','discount_type')
+
+# Hide Groups from admin sidebar
+try:
+    admin.site.unregister(Group)
+except NotRegistered:
+    pass
